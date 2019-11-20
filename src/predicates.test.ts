c17360699077ca8a0a87d682f1ffb6606525f10d
Hermann Pauly <hp@campudus.com>
2019-11-20 16:17:52 +0100

    Improve some predicates

---
+++ src/predicates.test.ts
@@ -0,0 +1,18 @@
+import { isEmpty } from "./predicates";
+
+describe("predicates", () => {
+  describe("isEmpty()", () => {
+    it("does as expected", () => {
+      expect(isEmpty([])).toBe(true);
+      expect(isEmpty({})).toBe(true);
+      expect(isEmpty("")).toBe(true);
+
+      expect(isEmpty("foo")).toBe(false);
+      expect(isEmpty(["foo"])).toBe(false);
+      expect(isEmpty({ foo: 1 })).toBe(false);
+      expect(isEmpty(1)).toBe(false);
+      expect(isEmpty(null as any)).toBe(false);
+      expect(isEmpty(undefined as any)).toBe(false);
+    });
+  });
+});
--- src/predicates.ts
+++ src/predicates.ts
@@ -1,9 +1,33 @@
-export const isNil = <T>(value: T): boolean => typeof value === undefined || value === null;
+import { Just, Nothing } from "./Maybe";
+import { Predicate } from "./types.d";
 
-export const isFunction = (value: any) => typeof value === "function";
+export const isNil: Predicate<any> = (value) => value === undefined || value === null;
 
-export const isNaN = (value: any): boolean => typeof value === "number" && isNaN(value);
+export const isFunction: Predicate<any> = (value) => typeof value === "function";
 
-export const isEmpty = <T>(coll: Array<T>): boolean => coll.length === 0;
+export const isNaN: Predicate<any> = (value) => typeof value === "number" && window.isNaN(value);
 
-export const isNotEmpty = <T>(coll: Array<T>): boolean => !isEmpty(coll);
+export const isString: Predicate<any> = (value) => typeof value === "string";
+
+export const isArray: Predicate<any> = (value) => value instanceof Array;
+
+export const isNumber: Predicate<any> = (value) =>
+  typeof value === "number" && !window.isNaN(value);
+
+export const isObject: Predicate<any> = (value) => typeof value === "object" && !isArray(value);
+
+export const isEmpty: Predicate<Array<any> | Object | string> = (coll): boolean =>
+  isNil(coll)
+    ? false
+    : (isArray(coll) && (coll as Array<any>).length === 0) ||
+      (isObject(coll) && Object.keys(coll).length === 0) ||
+      (isString(coll) && (coll as string) === "");
+
+export const isNotEmpty: Predicate<Array<any> | Object | string> = (coll): boolean =>
+  !isEmpty(coll);
+
+export const isJust: Predicate<any> = (value) => value instanceof Just;
+export const isNothing: Predicate<any> = (value) => value instanceof Nothing;
+export const isMaybe: Predicate<any> = (value) => isJust(value) || isNothing(value);
+
+export const isSome: Predicate<any> = (value) => !isNil(value) && !isNothing(value);
