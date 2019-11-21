e38f19f03b83d82f2f919b623e6d36199e7617e6
Hermann Pauly <hp@campudus.com>
2019-11-21 15:54:32 +0100

    Add Either monad

---
+++ src/Either.test.ts
@@ -0,0 +1,77 @@
+import { Left, Right, either, eitherIf, eitherNullable, EitherPattern } from "./Either";
+
+describe("Either", () => {
+  it("will get implemented", () => {
+    expect(true).toBe(true);
+  });
+
+  const throwError = (_: any) => {
+    throw new Error("BOOM!");
+  };
+
+  const dontThrow = (x: any) => x;
+
+  const good = eitherNullable<string>("result");
+  const bad = eitherNullable(null);
+
+  it("Can be constructed from different value", () => {
+    expect(either(1)).toBeInstanceOf(Right);
+    expect(either(null)).toBeInstanceOf(Right);
+
+    const pred = (x: number) => x > 2;
+    expect(eitherIf(pred)(5)).toBeInstanceOf(Right);
+    expect(eitherIf(pred)(2)).toBeInstanceOf(Left);
+
+    expect(eitherNullable(1)).toBeInstanceOf(Right);
+    expect(eitherNullable(null)).toBeInstanceOf(Left);
+
+    expect(eitherNullable(null).getReason()).toBeTruthy();
+  });
+
+  it("wraps values", () => {
+    expect(either(1).getValue()).toBe(1);
+    expect(either(null).getValue()).toBe(null);
+    expect(() => eitherNullable(null).getValue()).toThrow();
+
+    expect(eitherNullable(null).getReason()).toBeDefined();
+    expect(() => either(1).getReason()).toThrow();
+
+    expect(good.getValueOr("fallback")).toBe("result");
+    expect(bad.getValueOr("fallback")).toBe("fallback");
+  });
+
+  it("binds safely", () => {
+    expect(good.bind(dontThrow)).toBeInstanceOf(Right);
+    expect(good.bind(throwError)).toBeInstanceOf(Left);
+    expect(bad.bind(dontThrow)).toBeInstanceOf(Left);
+
+    expect(good.bind(throwError).getReason()).toEqual(new Error("BOOM!"));
+  });
+
+  it("wraps side effects", () => {
+    let n = 0;
+    const fxAdd = (_?: any) => n++;
+
+    expect(good.effect(fxAdd).getValue()).toBe("result");
+    expect(bad.effect(fxAdd)).toBeInstanceOf(Left);
+
+    expect(n).toBe(1);
+  });
+
+  it("works as branching", () => {
+    const pattern: EitherPattern<any, string> = {
+      left: () => "left branch",
+      right: (_: any) => "right branch",
+    };
+
+    expect(good.match(pattern).getValue()).toEqual("right branch");
+    expect(bad.match(pattern).getValue()).toEqual("left branch");
+    expect(
+      good
+        .match(pattern)
+        .bind(throwError)
+        .match(pattern)
+        .getValue()
+    ).toEqual("left branch");
+  });
+});
---
+++ src/Either.ts
@@ -0,0 +1,139 @@
+import { Effect } from "./types.d";
+import { Mappable, Predicate } from "./types";
+import { Maybe, just, nothing } from "./Maybe";
+import { Monad } from "./Monad";
+import { getMonadValue } from "./tools";
+import { isEither, isMonad, isNil, isSome } from "./predicates";
+
+export type Either<L, R> = Left<L, R> | Right<L, R>;
+export type Reason = Error | string;
+
+export type EitherPattern<A, B> = {
+  right: (value: A) => B;
+  left: () => B;
+};
+
+export class Right<L, R> implements Monad<R> {
+  private readonly value: R;
+  constructor(value: R | Monad<R>) {
+    this.value = getMonadValue(value);
+  }
+  bind<R2>(fn: Mappable<R, R2>): Either<Reason, R2> {
+    try {
+      const { value } = this;
+      return new Right<Reason, R2>(fn(value));
+    } catch (err) {
+      return new Left<Reason, R2>(err);
+    }
+  }
+  match<R2>(pattern: EitherPattern<R, R2>): Either<L, R2> {
+    try {
+      const { value } = this;
+      return new Right<Reason, R2>(pattern.right(value)) as any;
+    } catch (err) {
+      return new Left<Reason, R2>(err) as any;
+    }
+  }
+  getValue() {
+    return this.value;
+  }
+  getValueOr() {
+    return this.getValue();
+  }
+  getReason(): L {
+    throw new Error("Can't get reason from Right");
+  }
+  effect(fx: Effect<R>): Either<L, R> {
+    try {
+      const { value } = this;
+      fx(value);
+      return this;
+    } catch (err) {
+      return new Left<Reason, R>(err) as any;
+    }
+  }
+  isMonad() {
+    return true;
+  }
+  isLeft() {
+    return false;
+  }
+  isRight() {
+    return true;
+  }
+  toMaybe(): Maybe<R> {
+    return just(this.value);
+  }
+  toString() {
+    return `Right(${JSON.stringify(this.value)})`;
+  }
+}
+
+export class Left<L, R> implements Monad<R> {
+  private readonly reason: L;
+  constructor(reason: L) {
+    this.reason = reason;
+  }
+  bind<R2>(_: Mappable<R, R2>): Either<Reason, R2> {
+    return this as any;
+  }
+  match<R2>(pattern: EitherPattern<R, R2>): Either<L, R2> {
+    try {
+      return new Right<L, R2>(pattern.left());
+    } catch (err) {
+      return new Left<L, R2>(err as any);
+    }
+  }
+  getValue(): any {
+    throw new Error("Can't get value of Left");
+  }
+  getValueOr(alternative: R): R {
+    return alternative;
+  }
+  getReason(): L {
+    return this.reason;
+  }
+  effect(_: Effect<R>): Either<L, R> {
+    return this as any;
+  }
+  isMonad() {
+    return true;
+  }
+  isLeft() {
+    return true;
+  }
+  isRight() {
+    return false;
+  }
+  toMaybe(): Maybe<R> {
+    return nothing();
+  }
+  toString() {
+    return `Left(${JSON.stringify(this.reason)})`;
+  }
+}
+
+export const right = <L, R>(value: R | Monad<R>) => new Right<L, R>(getMonadValue(value));
+export const left = <L, R>(reason: L) => new Left<L, R>(reason);
+
+export const either = <L, R>(value: R | Monad<R>): Either<L, R> =>
+  isEither(value)
+    ? (value as Either<L, R>)
+    : isMonad(value)
+    ? right((value as Monad<R>).getValue())
+    : right(value as R);
+
+export const eitherIf = <Reason, R>(pred: Predicate<R>) => (value: R | Monad<R>) => {
+  const _value: R = (!isNil(value) ? getMonadValue(value) : value) as R;
+  try {
+    return pred(_value)
+      ? right<Reason, R>(value)
+      : left<Reason, R>(
+          `<${typeof _value}>${_value} did not satisfy predicate ${pred.name}` as any
+        );
+  } catch (err) {
+    return left<Reason, R>(err);
+  }
+};
+
+export const eitherNullable = <R>(value: R | Monad<R>) => eitherIf(isSome)(value);
