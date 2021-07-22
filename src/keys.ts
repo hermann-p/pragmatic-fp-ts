dcff11d3f94521a7d92ef7939eabbae277ca3c6e
Hermann <hp@campudus.com>
2021-07-22 11:48:26 +0200

    Extend `keys` to inlude maps

--- src/keys.ts
+++ src/keys.ts
@@ -1,5 +1,12 @@
-import { Dictionary, getValueOr } from "./main";
+import { getValueOr, isEmpty, isFunction } from "./main";
 
-export function keys(dict: Dictionary) {
-  return Object.keys(getValueOr({}, dict));
+export function keys<T extends Record<any, unknown>>(coll: T): string[];
+export function keys<T>(coll: Map<T, unknown>): T[];
+export function keys(input: unknown) {
+  const coll: any = getValueOr({}, input);
+  return isEmpty(coll)
+    ? []
+    : isFunction(coll.values)
+    ? Array.from(coll.values())
+    : Object.keys(coll);
 }
