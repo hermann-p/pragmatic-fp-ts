94190d6cfda56cefe710457c16d783daf2d578d4
Hermann <hp@campudus.com>
2021-07-22 11:48:50 +0200

    Improve `value` to include maps and sets

--- src/values.ts
+++ src/values.ts
@@ -1,5 +1,13 @@
-import { Dictionary, getValueOr } from "./main";
+import { Dictionary, isEmpty, isFunction, getValueOr } from "./main";
 
-export function values<T = any>(dict: Dictionary<T>) {
-  return Object.values(getValueOr({}, dict));
+export function values<T>(coll: Dictionary<T>): T[];
+export function values<T>(coll: Map<unknown, T>): T[];
+export function values<T>(coll: Set<T>): T[];
+export function values(dict: unknown) {
+  const coll: any = getValueOr({}, dict);
+  return isEmpty(coll as any)
+    ? []
+    : isFunction(coll.values)
+    ? Array.from(coll.values())
+    : Object.keys(coll);
 }
