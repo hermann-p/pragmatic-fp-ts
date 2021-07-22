06dad6dd3a9547131b95cbf1f5409e0ca1950bc0
Hermann <hp@campudus.com>
2021-07-22 16:15:36 +0200

    Loosen typing in InfiniteArray

--- src/tools/InfiniteArray.ts
+++ src/tools/InfiniteArray.ts
@@ -2,7 +2,7 @@ export function InfiniteArray<A>(calcElem: (idx: number) => A): A[] {
   return new Proxy([], {
     get(_, key) {
       if (key === "length") return Number.POSITIVE_INFINITY;
-      const idx = typeof key === "string" ? parseInt(key) : (key as number);
+      const idx = typeof key === "string" ? parseInt(key) : ((key as unknown) as number);
       return calcElem(idx);
     },
   });
