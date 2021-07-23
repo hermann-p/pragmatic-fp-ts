c172e1bce8be1f193183b1c21aa33d5d61fbc147
Hermann <hp@campudus.com>
2021-07-23 15:14:43 +0200

    Fix values function

--- src/values.ts
+++ src/values.ts
@@ -1,13 +1,13 @@
-import { Dictionary, isEmpty, isFunction, getValueOr } from "./main";
+import { Dictionary, isNil, isFunction, getValueOr } from "./main";
 
 export function values<T>(coll: Dictionary<T>): T[];
 export function values<T>(coll: Map<unknown, T>): T[];
 export function values<T>(coll: Set<T>): T[];
 export function values(dict: unknown) {
   const coll: any = getValueOr({}, dict);
-  return isEmpty(coll as any)
+  return isNil(coll as any)
     ? []
     : isFunction(coll.values)
     ? Array.from(coll.values())
-    : Object.keys(coll);
+    : Object.values(coll);
 }
