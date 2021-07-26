dd3ff9590647b33fac09580e528985fc3a30c9cb
Hermann <hp@campudus.com>
2021-07-26 11:02:19 +0200

    Add array to `values` input

--- src/values.ts
+++ src/values.ts
@@ -1,13 +1,16 @@
-import { Dictionary, isNil, isFunction, getValueOr } from "./main";
+import { Dictionary, getValueOr, isFunction, isNil } from "./main";
 
 export function values<T>(coll: Dictionary<T>): T[];
 export function values<T>(coll: Map<unknown, T>): T[];
 export function values<T>(coll: Set<T>): T[];
+export function values<T>(coll: T[]): T[];
 export function values(dict: unknown) {
   const coll: any = getValueOr({}, dict);
   return isNil(coll as any)
     ? []
     : isFunction(coll.values)
     ? Array.from(coll.values())
+    : Array.isArray(coll)
+    ? coll
     : Object.values(coll);
 }
